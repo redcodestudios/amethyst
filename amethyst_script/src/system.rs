c42954ae44348cd2b3075f61dbe32cdd8fadce76
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-02-04 17:52:04 -0300

    add script system to hello_script example

--- amethyst_script/src/system.rs
+++ amethyst_script/src/system.rs
@@ -1,37 +1,36 @@
 //!Script system
-use crate::driver::Driver;
+use crate::driver::{Driver, LuaDriver};
 use amethyst_core::ecs::System;
 use std::fs;
 
 
-pub struct ScriptSystem<'a, D: Driver> {
-    pub script_dir: &'a str,
-    pub driver: D
-}
+pub struct ScriptSystem;//<D: Driver> {
+    //pub script_dir: &'a str,
+    //pub driver: D
+//}
 
-impl<'a, D> ScriptSystem<'a, D>
-where
-    D: Driver
-{
-    pub fn new(script_dir: &'a str, driver: D) -> Self {
-        Self {
-            script_dir: script_dir,
-            driver: driver,
-        }
-    }
-}
+//impl<'a, D> ScriptSystem<'a, D>
+//where
+//    D: Driver
+//{
+//    pub fn new(script_dir: &'a str, driver: D) -> Self {
+//        Self {
+//            script_dir: script_dir,
+//            driver: driver,
+//        }
+//    }
+//}
 
-impl<'a, D> System<'a> for ScriptSystem<'a, D>
-where
-    D: Driver
+impl<'a> System<'a> for ScriptSystem
+//where
+//    D: Driver
 {
     type SystemData = ();
-
     fn run(&mut self, data: Self::SystemData){
-        let scripts_entries = fs::read_dir(self.script_dir.to_string()).unwrap();
+        let scripts_entries = fs::read_dir(String::from("examples/hello_script/scripts")).unwrap();
         for script_entry in scripts_entries {
             let script_path = String::from(script_entry.unwrap().path().to_str().unwrap());
-            self.driver.exec_script(script_path);
+            LuaDriver.exec_script(script_path);
         }
     }
 }
