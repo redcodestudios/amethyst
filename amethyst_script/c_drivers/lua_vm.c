284f98a6ea26f376c0e8a1bb142ec1e2dd160875
Rodrigo Oliveira <rodrigo.redcode@gmail.com>
2020-03-11 16:45:39 -0300

    add pong ball sprite and move the ball through script

--- amethyst_script/c_drivers/lua_vm.c
+++ amethyst_script/c_drivers/lua_vm.c
@@ -23,27 +23,41 @@ static int wrapper_log(lua_State *L) {
 
 static int wrapper_moveup(lua_State *L) {
     if (!lua_checkstack(L, 2)) {
-        printf("not enough arguments\n");
+        fprintf(stderr, "wrong amount of arguments\n");
         return 0;
     }
-    Transform* t = lua_touserdata(L, 1);
+
+    // pushes onto the top of the stack the value of the field `ref` of
+    // the table `self` that is the first arg of the function moveup
+    lua_getfield(L, 1, "ref");
+
+    // convert the value of `ref` that is on the top of the stack
+    // to a Transform reference of rust pointer
+    Transform* t = (Transform*) lua_touserdata(L, -1);
+    
     double amount = lua_tonumber(L, 2);
     move_up(t, (float) amount);
     return 1;
 }
 
+void create_transform_table_instance(lua_State *L, Transform* t) {
+    lua_newtable(L);
+    
+    lua_pushstring(L, "ref");
+    lua_pushlightuserdata(L, t);
+    lua_settable(L, -3);
+    
+    
+    lua_pushstring(L, "move_up");
+    lua_pushcfunction(L, wrapper_moveup);
+    lua_settable(L, -3);
+    
+    lua_setglobal(L, "Transform");
+}
+
 void call_lua(const char* script, Transform* t) {
-    //pwd();
     rust_log("DEU BOM");
 
-    //Transform* t = get_transform(components);
-    //Transform* t = transform;
-    //print_addr(t);
-    //printf("VM SAYS: address is : %p\n", t);
-    //printf("VM SAYS: transform y is %f\n", translation_y(t));
-    //move_up(t, 50);    
-    //printf("VM SAYS: now transform y is %f\n", translation_y(t));
-    
     lua_State *L;
     L = luaL_newstate();
     printf("C: loading lua script %s\n", script);
@@ -52,19 +66,12 @@ void call_lua(const char* script, Transform* t) {
     lua_pushcfunction(L, wrapper_log);
     lua_setglobal(L, "rust_log");
 
-    lua_pushcfunction(L, wrapper_moveup);
-    lua_setglobal(L, "move_up");
-    
-    lua_pushlightuserdata(L, t);
-    lua_setglobal(L, "Transform");
+    create_transform_table_instance(L, t);
     
     luaL_loadfile(L, script);
 
     if (lua_pcall(L, 0, 0, 0))
         printf("C: falhou: %s\n", lua_tostring(L, -1));
     
-    lua_getglobal(L, "Transform");
-    t = (Transform*)lua_touserdata(L, -1);
-    //*transform = *t;
     lua_close(L);
 }
